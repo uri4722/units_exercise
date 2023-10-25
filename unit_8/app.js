da20bc05302df956cc11eab0ee6eabb6ab440483
uri4722 <132268605+uri4722@users.noreply.github.com>
2023-10-25 17:51:50 +0300

    u

---
+++ unit_8/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const baseUrl = "/numbers"
+
+app.get(baseUrl, (req, res) => {
+    res.send('success using get')
+});
+app.post(baseUrl, (req, res) => {
+    res.send('success using post')
+});
+app.put(baseUrl, (req, res) => {
+    res.send('success using put')
+});
+app.delete(baseUrl, (req, res) => {
+    res.send('success using delete')
+});
+
+process.env.PORT = 7500;
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log("listen to port" + PORT))
