c69ff048de71082b2f23aa864d3b6c353b415831
uri4722 <132268605+uri4722@users.noreply.github.com>
2023-10-31 21:22:27 +0200

    cors

--- unit_10/react_node_app/server/index.js
+++ unit_10/react_node_app/server/index.js
@@ -7,9 +7,7 @@ const app = express();
 const database = "this the contect to came from the database"
 
 const PORT = process.env.PORT || 3001;
-app.use((req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*")
-})
+app.use(cors());
 app.use(express.static(path.join(path.dirname(__dirname), 'client', 'build')));
 
 app.get('/', (req, res) => {
